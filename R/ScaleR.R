321d2a4552c31f9e2ed8504c66664125dd007bcf
KristoferLintonReid <50206873+KristoferLintonReid@users.noreply.github.com>
2020-02-19 11:18:42 +0000

    Update, Additions: plots, default inter, results
    Plot added of R2 and Q2 against scaling factor.
    Default interval set 
    training and test split pre model
    PLS updates 
    Returns list with results and test data for further applications

--- R/ScaleR.R
+++ R/ScaleR.R
@@ -1,14 +1,16 @@
 #' @title  ScaleR: An R package for investigating scaling factors effects on models
 #'
-#' @description This package employs Ridge regression or PLS over multiple scaling factors.
+#' @description This package employs Ridge regression or PLS over multiple scaling factors. Default is PLS
 #'
 #' @param x is a data matrix of predictors, features in columns and samples in rows.
 #'
 #' @parma y is the outcome variable.
 #'
-#' @param inter is the Scalaing factor intervals, e.g 0.1, 0.05, 0.01.
+#' @param inter is the Scalaing factor intervals, e.g 0.1, 0.05, 0.01. For instance
 #'
 #' @param methods is the model type you want to employ (methods='Ridge), default is PLS.
+#'
+#' @param plot this determines if you rewuire a plot of R" and Q2 against scaling factor, default is TRUE
 #'
 #' @return matrix of correspongin model assesment metrics
 #'
@@ -19,21 +21,44 @@
 
 
 
+ScaleR <- function(x,y, method='PLS', inter=NULL, plot=TRUE){
 
-
-
-
-
-
-
-ScaleR <- function(x,y, method, inter ,plots){
-  Scaling_Factor = seq(0,1, inter)
-  Data_Sets <- SCALE(x)
+  ### Test train 80/20 split
+  if (is.null(inter)){print('interval not provided, using default value of 0.1')
+    inter=0.1}
+  x <- as.data.frame(x)
+  y <- as.data.frame(y)
+  if (nrow(x)==0){
+    print('Must provide x as a numeric Data Set')
+  }
+  if (nrow(x)!=nrow(y)){
+    print('Must provide x and y with same number of samples/rows')
+  }
+  trainingIndex <- sample(1:nrow(x), 0.8*nrow(x)) # indices for 80% training data ### can edit this/ take the training and test spilit out of for loop incase we need directly comparable results
+  trainingData <- x[trainingIndex, ] # training data
+  testData <- x[-trainingIndex, ] # test data
+  trainingY <- y[trainingIndex, ] ## note the use of index for the split nrow xtrain= nrow ytrain ##
+  testY <- y[-trainingIndex, ] # test data
+  inter <- inter
+  Scaling_Factor <- seq(0,1, as.numeric(inter))### Scaling_Factor
+  Data_Sets <- SCALE(trainingData, Scaling_Factor) ### Data_Sets of initial training with diffrent scalings
 
   if (method=='Ridge') {
     print('Employing Ridge Regression')
-    Ridge(Data_Sets, y)}
-  else {
+    Ridge(Data_Sets, trainingY)}
+  if (method=='PLS') {
     print('Employing PLS')
-    res<- PLS(Data_Sets, y) }
-  return(res)}
+    Res <- PLS(Data_Sets, trainingY)
+    Res$Scaling_Factor <- Scaling_Factor
+    if (plot){
+      plot(as.numeric(Res$Scaling_Factor),as.numeric(Res$RSquared),type="l",col="red", xlim=c(0, 1), ylim=c(0, max(as.numeric(Res$RSquared)+0.1)), xlab='', ylab='')
+      par(new=TRUE)
+      lines(as.numeric(Res$Scaling_Factor),as.numeric(Res$QSquared), col='green')
+      title(main="Scaling Factor vs R2 and Q2",
+            xlab="Scaling Factor ", ylab="R2 & Q2")
+      par(xpd=TRUE)
+      legend("topright", title="Metric",
+             c("R2","Q2"), fill=c('red','green'))}} ###
+  Res <- list(Res, testY, trainingY )
+  names(Res) <- c('Results', 'Test_Y', 'Training_Y')
+  return(Res)}
