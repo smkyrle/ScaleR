b0a38e17158b6bdad3636c2e31021a1403f79a91
KristoferLintonReid <50206873+KristoferLintonReid@users.noreply.github.com>
2020-02-19 11:15:17 +0000

    Update function, scaling factor bug

--- R/Scale.R
+++ R/Scale.R
@@ -1,9 +1,9 @@
-SCALE <- function(x) {
+SCALE <- function(d, Scaling_Factor) {
   Data_Sets <- list()
   for (i in Scaling_Factor){
-    x.centered <- apply(x, 2, function(x) x - mean(x))
+    x.centered <- apply(d, 2, function(x) x - mean(x)) ### x-mean(x)
     # Then we perform scaling on the mean-centered matrix
-    x.sc <- apply(x.centered, 2, function(x) x/(sd(x)^i))
+    x.sc <- apply(x.centered, 2, function(x) x/(sd(x)^i)) ## new x/sd raised to the power of sclaing factor
     Data_Sets[[paste0("Factor_", i)]] <- x.sc
   }
   return(Data_Sets)
