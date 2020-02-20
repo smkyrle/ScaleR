670eb3851b4ca49fdf3af80f61fc7dc5638ed1d8
KristoferLintonReid <50206873+KristoferLintonReid@users.noreply.github.com>
2020-02-20 13:05:10 +0000

    Updated with Ridge Regression

--- R/model_functions.R
+++ R/model_functions.R
@@ -1,31 +1,40 @@
+#### Ridge Function
 Ridge <-function(x, y){
   RSquare <- list()
-  # Q2 <- list()
-  RMSE <- list()
-  lambda <- list()
+  CV_RMSE <- list()
+  lambda_out <- list()
   res <- list()
+  QSquare <- list()
   for (i in seq_along(x)) {
     set.seed(123)
-    inputData <- x[i]
-    inputData <- as.data.frame(inputData)
-    trainingIndex <- sample(1:nrow(inputData), 0.8*nrow(inputData)) # indices for 80% training data ### can edit this/ take the training and test spilit out of for loop incase we need directly comparable results
-    trainingData <- inputData[trainingIndex, ] # training data
-    testData <- inputData[-trainingIndex, ] # test data
-    training_Y <- y[trainingIndex, ] ## note the use of index for the split nrow xtrain= nrow ytrain ##
-    testY <- y[-trainingIndex, ]
+    train.data <- as.matrix(x[[i]]) ### One of the training Data sets extrpolated
     lambdas <- 10^seq(3, -2, by = -.1) ### glmnet has some bult in lambda range. Here we have values ranging from λ=1010 to λ=10−2, essentially covering the full range of scenarios from the null model containing only the intercept, to the least squares fit.
-    cv.out = cv.glmnet(x=data.matrix(trainingData), y=training_Y, alpha = 0) # Fit ridge regression model on training data, default 10 fold crossvalidation
+    cv.out = glmnet::cv.glmnet(x=train.data, y=as.matrix(y), lambda=lambdas, keep=TRUE, type.measure="mse", alpha = 0, standardize= FALSE) # Fit ridge regression model on training data, default 10 fold crossvalidation
     bestlam = cv.out$lambda.min  # Select lamda that minimizes training MSE
-    predictions <- predict(cv.out, s = bestlam, data.matrix(testData))  # predict on test data
-    RootMSE = caret::RMSE(predictions, testY)  ## RMSE test
-    Rsquared = caret::R2(predictions, testY)   ## R-Squared test
-    RSquare[[paste0("Factor_", i)]] <- Rsquared ## need to figure out what is going on with R2 and Q2 in R
-    RMSE[[paste0("Factor_", i)]] <- RootMSE
-    lambda[[paste0("Factor_", i)]] <- cv.out$lambda.min
-    res <- do.call(cbind, list(RSquare, RMSE, lambda)) ## #bind all lists into results
+    mse <- min(cv.out$cvm) ### best lambda corresponds to min  cvm, mse.
+
+    RSquare[[paste0("Factor_", i)]] <- r_squared(as.matrix(y), as.matrix(predict(cv.out, newx = train.data, s = cv.out$lambda.min)))
+
+    CV_RMSE[[paste0("Factor_", i)]] <- sqrt(mse)
+    lambda_out[[paste0("Factor_", i)]] <- cv.out$lambda.min
+
+    Q22 <- lapply(unique(cv.out$foldid), function(id) {
+      fit <- glmnet(x = train.data[cv.out$foldid != id,], ### this part is complicated, crossvalidiating the foldid, Fit excluding test set (foldid == id)
+                    y = y[cv.out$foldid != id],
+                    alpha = 0, standardize=FALSE)
+      ## Test-set Y_hat using model fit at best lambda
+      y_pred <- predict(fit, newx = train.data[cv.out$foldid == id,], s = cv.out$lambda.min)
+      ## Test-set Y
+      y_test <- y[cv.out$foldid == id]  ##  ## Test-set Y
+      Q22 <- r_squared(y_test, y_pred) ## Cross-validated outer R2/ Q2
+      return(Q22)})
+    QSquare[[paste0("Factor_", i)]] <- mean(as.numeric(Q22)) ## Q22 lapply fun(id) calulates Q2 for ever fold, now we take the mean
+
+    res <- do.call(cbind, list(RSquare, QSquare, CV_RMSE, lambda_out)) ## #bind all lists into results
     res <- as.data.frame(res)
-    colnames(res) <- c('RSquare', 'RMSE', 'lambda')
+    colnames(res) <- c('RSquared_Y','QSquared_Y', 'CV_RMSE', 'lambda_min')
   }
+
   return(res)
 }
 
@@ -50,7 +59,7 @@ PLS <- function(x, y){
     model_out[[paste0("Factor_",i)]] <- model ### save each model iteritviely
     res <- do.call(cbind, list(RSquare, QSquare, NComponents, CV_RMSEP, model_out)) ## #bind all lists into results
     res <- as.data.frame(res)
-    colnames(res) <- c('RSquared', 'QSquared', 'NComponents', 'CV_RMSEP', 'model')
+    colnames(res) <- c('RSquared_Y', 'QSquared_Y', 'NComponents', 'CV_RMSEP', 'model')
   }
   return(res)
 }
