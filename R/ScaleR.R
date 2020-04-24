4db45704866add2f8e6bfbc27f79c1ce56ced15b
KristoferLintonReid <50206873+KristoferLintonReid@users.noreply.github.com>
2020-04-24 12:01:52 -0400

    Updated main function
    seed set enabled with a default of 1234
    kth model return enabled- will select best performing models according to RCV

--- R/ScaleR.R
+++ R/ScaleR.R
@@ -10,7 +10,11 @@
 #'
 #' @param methods is the model type you want to employ (methods='Ridge), default is PLS.
 #'
-#' @param plot this determines if you rewuire a plot of R" and Q2 against scaling factor, default is TRUE
+#' @param plot this determines if you require a plot of R2Y and Q2Y against scaling factor, default is TRUE
+#'
+#' @param seed this will allow user to define a seed number if not provided will use the default 1234 
+#'
+#'@ param k is the number of best perforing models according to Robustness of Cross Validation (R2Y/Q2Y)
 #'
 #' @return matrix of correspongin model assesment metrics
 #'
@@ -19,60 +23,79 @@
 #' @export
 
 
+ScaleR <- function(x,y, method='PLS', inter=NULL, plot=TRUE, seed=NULL, k=NULL){
+    
+    ## Initital checks inter, data sets etc. ## 
+    if (is.null(inter)){print('interval not provided, using default value of 0.1')
+        inter=0.1} 
+    x <- as.data.frame(x)
+    y <- as.data.frame(y)
+    if (nrow(x)==0){
+        print('Must provide x as a numeric Data Set')
+    }
+    if (nrow(x)!=nrow(y)){
+        print('Must provide x and y with same number of samples/rows')
+    }
+    ## set seed to default if null/not privided##
+    if (is.null(seed)){
+        seed=1234}
+    seed <- as.numeric(seed)
+    y<- as.matrix(y)
+    trainingData <- as.matrix(x)
+    inter <- inter     
+    Scaling_Factor <- seq(0,1, as.numeric(inter))### Scaling_Factor 
+    Data_Sets <- SCALE(trainingData, Scaling_Factor) ### Data_Sets of initial training with diffrent scalings 
+    str(Data_Sets)
+    
 
-
-ScaleR <- function(x,y, method='PLS', inter=NULL, plot=TRUE){
-
-  ### Test train 80/20 split
-  if (is.null(inter)){print('interval not provided, using default value of 0.1')
-    inter=0.1}
-  x <- as.data.frame(x)
-  y <- as.data.frame(y)
-  if (nrow(x)==0){
-    print('Must provide x as a numeric Data Set')
-  }
-  if (nrow(x)!=nrow(y)){
-    print('Must provide x and y with same number of samples/rows')
-  }
-  y<- as.matrix(y)
-  set.seed(123)
-  trainingIndex <- sample(1:nrow(x), 0.8*nrow(x)) # indices for 80% training data ### can edit this/ take the training and test spilit out of for loop incase we need directly comparable results
-  trainingData <- x[trainingIndex, ] # training data
-  testData <- x[-trainingIndex, ] # test data
-  trainingY <- y[trainingIndex, ] ## note the use of index for the split nrow xtrain= nrow ytrain ##
-  testY <- y[-trainingIndex, ] # test data
-  inter <- inter
-  Scaling_Factor <- seq(0,1, as.numeric(inter))### Scaling_Factor
-  Data_Sets <- SCALE(trainingData, Scaling_Factor) ### Data_Sets of initial training with diffrent scalings
-
-  if (method=='Ridge') {
-    print('Employing Ridge Regression')
-    Res <- Ridge(Data_Sets, trainingY)
-    Res$Scaling_Factor <- Scaling_Factor
+    if (method=='Ridge') {
+                print('Employing Ridge Regression')
+                Res <- Ridge(Data_Sets, y)
+                Res$Scaling_Factor <- Scaling_Factor
+                Res$RCV <- as.numeric(Res$RSquared_Y)/as.numeric(Res$QSquared_Y)
+                
     if (plot){
-      plot(as.numeric(Res$Scaling_Factor),as.numeric(Res$RSquared_Y),type="l",col="red", xlim=c(0, 1), ylim=c(0, max(as.numeric(Res$RSquared_Y)+0.1)), xlab='', ylab='')
-      par(new=TRUE)
-      lines(as.numeric(Res$Scaling_Factor),as.numeric(Res$QSquared_Y), col='green')
-      title(main="Scaling Factor vs R2Y and Q2Y",
-            xlab="Scaling Factor ", ylab="R2Y & Q2Y")
-      par(xpd=TRUE)
-      legend("topright", title="Metric",
-             c("R2","Q2"), fill=c('red','green'))}} ###
+            plot(as.numeric(Res$Scaling_Factor),as.numeric(Res$RSquared_Y),type="l",col="red", xlim=c(0, 1), ylim=c(0, max(as.numeric(Res$RSquared_Y)+0.1)), xlab='', ylab='')
+            par(new=TRUE)
+            lines(as.numeric(Res$Scaling_Factor),as.numeric(Res$QSquared_Y), col='green')
+            title(main="Scaling Factor vs R2 and Q2",
+            xlab="Scaling Factor ", ylab="R2 & Q2")
+            par(xpd=TRUE)
+            legend("topright", title="Metric",
+            c("R2","Q2"), fill=c('red','green'))}### 
+    
+    if (k>0 & method=='Ridge'){
+            k.RCV <- Ind.RCV.k(Res$RCV, k)
+            Data.k <- Data_Sets[c(k.RCV)]
+            str(Data.k)
+            model <- Ridge.k(Data.k, y)
+            Res <- as.list(Res)
+            Res$model <- model}}
+    
+            
 
-  if (method=='PLS') {
-    print('Employing PLS')
-    Res <- PLS(Data_Sets, trainingY)
-    Res$Scaling_Factor <- Scaling_Factor
+    if (method=='PLS') {
+                print('Employing PLS')
+                Res <- PLS(Data_Sets, y)
+                Res$Scaling_Factor <- Scaling_Factor
+                Res$RCV <- as.numeric(Res$RSquared_Y)/as.numeric(Res$QSquared_Y)
+
     if (plot){
-      plot(as.numeric(Res$Scaling_Factor),as.numeric(Res$RSquared_Y),type="l",col="red", xlim=c(0, 1), ylim=c(0, max(as.numeric(Res$RSquared_Y)+0.1)), xlab='', ylab='')
-      par(new=TRUE)
-      lines(as.numeric(Res$Scaling_Factor),as.numeric(Res$QSquared_Y), col='green')
-      title(main="Scaling Factor vs R2Y and Q2Y",
-            xlab="Scaling Factor ", ylab="R2Y & Q2Y")
-      par(xpd=TRUE)
-      legend("topright", title="Metric",
-             c("R2","Q2"), fill=c('red','green'))}} ###
-  Res <- list(Res, testY, trainingY )
-  names(Res) <- c('Results', 'Test_Y', 'Training_Y')
-  return(Res)
+            plot(as.numeric(Res$Scaling_Factor),as.numeric(Res$RSquared_Y),type="l",col="red", xlim=c(0, 1), ylim=c(0, max(as.numeric(Res$RSquared_Y)+0.1)), xlab='', ylab='')
+            par(new=TRUE)
+            lines(as.numeric(Res$Scaling_Factor),as.numeric(Res$QSquared_Y), col='green')
+            title(main="Scaling Factor vs R2 and Q2",
+            xlab="Scaling Factor ", ylab="R2 & Q2")
+            par(xpd=TRUE)
+            legend("topright", title="Metric",
+            c("R2","Q2"), fill=c('red','green'))}
+        
+        if (k>0 & method=='PLS'){
+            k.RCV <- Ind.RCV.k(Res$RCV, k)
+            Data.k <- Data_Sets[c(k.RCV)]
+            str(Data.k)
+            model <- PLS.k(Data.k, y)
+            Res <- as.list(Res)
+            Res$model <- model}} ### 
+return(Res)
 }
