47113b2337268e57a7b3d1e4332b49a2f3576259
KristoferLintonReid <50206873+KristoferLintonReid@users.noreply.github.com>
2020-06-12 12:39:04 -0400

    Update model_functions.R

--- R/model_functions.R
+++ R/model_functions.R
@@ -94,31 +94,27 @@ return(res)
     }
 
 ### PLS function 
-PLS <- function(x, y){ 
-    NComponents = list() ## define key outputs as lists first to save each for loop iteration
-    RSquare <- list() ## ^
-    QSquare <- list()
-    RMSE <- list()
-    res <- list()
-for (i in seq_along(x)) {
-            set.seed(seed_val)
-            inputData <- x[i]
-            inputData <- as.data.frame(inputData)
-            trainingIndex <- sample(1:nrow(inputData), 0.8*nrow(inputData)) # indices for 80% training data ### can edit this/ take the training and test spilit out of for loop incase we need directly comparable results
-            trainingData <- inputData[trainingIndex, ] # training data
-            testData <- inputData[-trainingIndex, ] # test data
-            training_Y <- y[trainingIndex, ] ## note the use of index for the split nrow xtrain= nrow ytrain ##
-            testY <- y[-trainingIndex, ]
-            model <- plsRglm::plsR(dataX = trainingData, dataY=trainingY, typeVC = 'standard')
-
-            predicto <- predict(model, testData)
-            #RSquare[[paste0("Factor_",i)]] = matrixpls::q2(predicto, testY, model = NULL)
-            NComponents[[paste0("Factor_",i)]] = model$bestTune
-            #QSquare[[paste0("Factor_",i)]]  <- pls::R2(predicto) 
-            res <- do.call(cbind, list(RSquare, QSquare, NComponents)) ## #bind all lists into results
-            res <- as.data.frame(res)
-            colnames(res) <- c('RSquare', 'NComponents')
-        }
+PLS <- function(x, y, seed_val){ 
+NComponents = list() ## define key outputs as lists first to save each for loop iteration
+RSquare <- list() ## ^
+QSquare <- list()
+model_out <- list()
+res <- list()
+CV_RMSEP <- list()
+    for (i in seq_along(x)) {
+                    set.seed(seed_val)
+                    train.data <- x[[i]] ### One of the training Data sets extrpolated 
+                    model <- pls::plsr(as.matrix(y)~as.matrix(train.data), data=as.data.frame(train.data), validation = "CV") ## validation-cv, 10 fold cross val
+                    CVRMSEP <- pls::RMSEP(model,'CV')$val ### cross-model-validated RMSEP calculated from the training data set- as not to leak info 
+                    NComp <- which.min(CVRMSEP) ## index of minimun CVRMSEP to detemine optimal number of components 
+                    RSquare[[paste0(names(x[i]))]] <- pls::R2(model, 'train')$val[as.numeric(NComp)] ## Take R2 for optimal no. comps  
+                    QSquare[[paste0(names(x[i]))]]  <- pls::R2(model, 'CV')$val[as.numeric(NComp)] ## Q2 also known as cross-validated R2
+                    CV_RMSEP[[paste0("Factor_",i)]]<- min(CVRMSEP)
+                    NComponents[[paste0("Factor_",i)]]<- as.numeric(NComp)## save no.componenst 
+                    res <- do.call(cbind, list(RSquare, QSquare, NComponents, CV_RMSEP)) ## #bind all lists into results
+                    res <- as.data.frame(res)
+                    colnames(res) <- c('RSquared_Y', 'QSquared_Y', 'NComponents', 'CV_RMSEP')
+}
 return(res)
     }
 
